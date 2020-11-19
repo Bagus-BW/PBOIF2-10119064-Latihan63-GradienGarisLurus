c9222f185c9955871a75a26be335b37c97dd0d6a
Bagus_24 <Bagus_24@Bagus24>
2020-11-19 11:36:53 +0700

    new commit

---
+++ src/pboif2/pkg10119064/latihan63/gradiengarislurus/PBOIF210119064Latihan63GradienGarisLurus.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan63.gradiengarislurus;
+
+import model.Koordinat;
+
+/**
+ *
+ * @author Bagus_24
+ */
+public class PBOIF210119064Latihan63GradienGarisLurus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //inisialisasi objek gradien - 1
+        Koordinat koordinat1 = new Koordinat(2, 10, 5, 7);
+        System.out.println("Garis yang melalui titik (" + koordinat1.getX1() + "," 
+                                                        + koordinat1.getY1() + ") dan (" 
+                                                        + koordinat1.getX2() + "," 
+                                                        + koordinat1.getY2() + ")");
+        System.out.println("memiliki gradien sebesar " + koordinat1.hitungGradien());
+        
+        System.out.println();
+        
+        //inisialisasi objek gradien - 2
+        Koordinat koordinat2 = new Koordinat(5, 1, 3, 12);
+        System.out.println("Garis yang melalui titik (" + koordinat2.getX1() + "," 
+                                                        + koordinat2.getY1() + ") dan (" 
+                                                        + koordinat2.getX2() + "," 
+                                                        + koordinat2.getY2() + ")");
+        System.out.println("memiliki gradien sebesar " + koordinat2.hitungGradien());
+        
+    }
+    
+}
